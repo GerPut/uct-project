576733a7387e703fd725912da3202d2f5a40daab
Gerhard <gerhard.putter@yahoo.com>
2022-03-20 19:26:21 +0200

    Move init Aos.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-// Animate on Scroll
-AOS.init();
 
 // Navbar
 const lineMenu = document.querySelector(".lines-menu");
 const navbar = document.querySelector(".navbar");
 
 lineMenu.addEventListener("click", () => {
-    navbar.classList.toggle("rotatechange");
-    lineMenu.classList.toggle('rotatechange')
-    lineMenu.classList.toggle("rotate")
+    navbar.classList.toggle("drop");
+    lineMenu.classList.toggle("rotatechange")
+    lineMenu.classList.toggle("spin")
 });
 // End of Navbar
+
+// Animate on Scroll
+AOS.init();
